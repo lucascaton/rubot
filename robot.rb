afee66185e4e8609dbdf014462dd538a910abab2
Lucas Caton <lucascaton@gmail.com>
2015-02-21 19:57:19 +1000

    Making the robot smarter

--- robot.rb
+++ robot.rb
@@ -2,12 +2,17 @@ class Robot
   NAMES = ['Sonny', 'Mokobot', 'Rubber', 'Scrappy', 'Copper', 'Tinker', 'Isetx', 'Grezzer', 'Combot',
     'Cyd', 'Adeoid', 'Evix', 'Spencer']
 
-  FORMAL_GREETINGS = [
-    /hi/
-  ]
+  REGEXS = {
+    formal_greetings: [
+      /\Ahi\z/
+    ], introductions: [
+      /\Amy name is (.+)\z/,
+      /\Ahi, my name is (.+)\z/
+    ]
 
-  # INFORMAL_GREETING_REGEXS = []
-  # BOGAN_GREETING_REGEXS    = []
+    # informal_greeting: []
+    # bogan_greeting:    []
+  }
 
   attr_reader :name
 
@@ -21,14 +26,34 @@ class Robot
     }
 
     def receive_message(message)
-      [FORMAL_GREETINGS].flatten.each do |regex|
+      message = message
+        .gsub(/\t|\n/, '')
+        .gsub(/(\.\.?{2}|,|;|\??!|!\?)\z/, '')
+        .gsub(/ +/, ' ').strip
+
+      [REGEXS[:formal_greetings]].flatten.each do |regex|
         if message.match(regex)
           @talked_subjects[:waiting_for_name] = true
           return "Hi human, what's your name?"
         end
       end
 
+      if @talked_subjects[:waiting_for_name]
+        REGEXS[:introductions].each do |regex|
+          if match = message.match(regex)
+            self.human_name = match.captures.first
+          end
+        end
+
+        self.human_name = message unless @human_name
+        return "Hi #{@human_name}, nice to meet you."
+      end
+
       "Sorry, I don't know what you mean."
     end
+  end
+
+  def human_name=(human_name)
+    @human_name = human_name.split.first.capitalize
   end
 end
--- spec/integrations/talk_spec.rb
+++ spec/integrations/talk_spec.rb
@@ -1,11 +1,41 @@
 RSpec.describe 'Talk' do
   subject { Robot.new }
 
-  it 'basic conversation' do
-    receive_message 'hi', and_return: "Hi human, what's your name?"
+  context 'starting a conversation' do
+    ['.', '...', ',', ';', '!', '!?', '?!'].each do |punctuation|
+      it "understand when using '#{punctuation}' in the end of the sentence" do
+        receive_message "hi#{punctuation}",  and_return: "Hi human, what's your name?"
+      end
+
+      it "understand when using a space plus a '#{punctuation}' in the end of the sentence" do
+        receive_message "hi #{punctuation}", and_return: "Hi human, what's your name?"
+      end
+    end
+
+    context 'when introducing' do
+      [
+        'lucas',
+        'my name is lucas',
+        'hi, my name is lucas',
+      ].each do |introducing_message|
+        context "with '#{introducing_message}'" do
+          it 'learns human name, replying with captalized name' do
+            receive_message 'hi', and_return: "Hi human, what's your name?"
+            receive_message introducing_message, and_return: "Hi Lucas, nice to meet you."
+          end
+        end
+      end
+
+      it 'learns only first human name' do
+        receive_message 'hi', and_return: "Hi human, what's your name?"
+        receive_message 'lucas caton', and_return: "Hi Lucas, nice to meet you."
+      end
+    end
   end
 
-  it 'no sense conversation' do
-    receive_message 'foobar', and_return: "Sorry, I don't know what you mean."
+  context 'when the message does not make sense' do
+    it 'replies correctly' do
+      receive_message 'foobar', and_return: "Sorry, I don't know what you mean."
+    end
   end
 end
