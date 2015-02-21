59e93df1524e91e08ba462e1652e72e52c35296f
Lucas Caton <lucascaton@gmail.com>
2015-02-21 18:35:07 +1000

    Updating RSpec configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 Dir[File.expand_path('../../*.rb', __FILE__)].each { |file| require file }
+
+RSpec.configure do |config|
+  config.order = 'random'
+  config.color = true
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+end
